d663c4cd28ebfd3e8873f54c562d25b229829d94
hisa <77286420+common-member@users.noreply.github.com>
2024-03-31 10:24:22 -0700

    Add files via upload

---
+++ kadai_008/calc.js
@@ -0,0 +1,26 @@
+let num = 15;
+
+if (num % 5 == 0 && num % 3 == 0) {
+    console.log("3と5の倍数です");
+} else if (num % 5 == 0) {
+    console.log("5の倍数です");
+} else if (num % 3 == 0) {
+    console.log("3の倍数です");
+} else {
+    console.log(num);
+}
+
+switch (true) {
+  case num % 5 == 0 && num % 3 == 0:
+    console.log("3と5の倍数です");
+    break;
+  case num % 5 == 0:
+    console.log("5の倍数です");
+    break;
+  case num % 3 == 0:
+    console.log("3の倍数です");
+    break;
+  default:
+    console.log(num);
+    break;
+}
